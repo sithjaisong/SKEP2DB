d0ede589c0315169af5b7e85dfb0394c008102e1
sithjaisong <sith.jaisong@gmail.com>
2016-03-27 16:40:30 +0800

    update 2

--- collect_database.R
+++ collect_database.R
@@ -34,9 +34,37 @@ SystemicData <- getSystemicData(SKEPdb)
 
 WeedData <- getWeedData(SKEPdb)
 
+
+# table 1
+active_ingr <- tbl(SKEPdb, "active_ingr")
+# table 2
+cem_type <- tbl(SKEPdb, "cem_type")
+# table 3
+crop_estab_type <- tbl(SKEPdb, "crop_estab_met")
+# table 4
+rice_var_type <- tbl(SKEPdb, "rice_var_type")
+# table 5 
+season_type <- tbl(SKEPdb, "seasons")
+# table 6
+soil_prop_type <- tbl(SKEPdb, "soil_prob_type")
+# table 7
+water_status_type <- tbl(SKEPdb, "water_status")
+# table 8
+weed_list <- tbl(SKEPdb, "weed_list")
+# table 9
+dev_stage <- tbl(SKEPdb, "devel_stage")
+# table 10
+weed_mgnt_type <- tbl(SKEPdb, "weed_mgnt_type")
+# table 11
+weed_type <- tbl(SKEPdb, "weed_type")
+
+
+
 # 9.Save in Rdata
-filepath <- file.path("C:", "Users", "sjaisong", "Documents", "GitHub", "SKEP2db", "SKEP2database.RData" )
+filepath <- file.path("C:", "Users", "sjaisong", "Google Drive", "surveySKEP1", "SKEP2database.RData" )
 filepath
 
-save(FieldData, FertilizerData, PestManagementData, YieldData, InjuriesData, SystemicData, WeedData, file = filepath)
+save(FieldData, FertilizerData, PestManagementData, YieldData, InjuriesData, SystemicData, WeedData,
+     active_ingr, cem_type, crop_estab_type, rice_var_type, season_type, soil_prop_type, water_status_type, weed_list, dev_stage, weed_mgnt_type, weed_type,
+     file = filepath)
 
---
+++ summary.R
@@ -0,0 +1,6 @@
+
+
+# yield 
+# Yield_synthesis <-
+        YieldData %>%  dplyr::select(id, main_id, area, fresh_w, moisture)
+        
